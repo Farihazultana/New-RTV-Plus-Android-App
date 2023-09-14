0eea4fb3580ba4be601b098cc1b85df1575d6a5e
Fariha Sultana <farihasultana237@gmail.com>
2023-09-14 19:35:26 +0600

    Updated SplashScreenActivity

--- app/src/main/java/com/example/rtv_plus_android_app_revamp/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/rtv_plus_android_app_revamp/ui/activities/SplashScreenActivity.kt
@@ -4,18 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rtv_plus_android_app_revamp.R
-import java.util.logging.Handler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val SPLASH_DELAY: Long = 2000
+        val SPLASH_DELAY: Long = 3000
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        android.os.Handler().postDelayed({
-            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java) // Replace with your main activity class
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(SPLASH_DELAY)
+
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, SPLASH_DELAY)
+        }
+
     }
 }
