a5548c4d8636e33b77ef45875f4ce1f9ed9f8442
Eatiak <easourav@gmail.com>
2023-09-14 14:21:07 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "RTV-Plus-Android-App-Revamp"
+include(":app")
+ 
