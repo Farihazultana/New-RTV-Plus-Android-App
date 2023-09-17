0d9b9964de690e5c81913d16304a697d7cf71cd0
Fariha Sultana <farihasultana237@gmail.com>
2023-09-17 16:14:04 +0600

    Lottie Loading animation added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,10 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
+
+    // lottie animation
+    implementation ("com.airbnb.android:lottie:5.2.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/rtv_plus_android_app_revamp/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/rtv_plus_android_app_revamp/ui/activities/SplashScreenActivity.kt
@@ -1,11 +1,8 @@
 package com.example.rtv_plus_android_app_revamp.ui.activities
 
 import android.content.Intent
-import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
 import com.example.rtv_plus_android_app_revamp.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -14,15 +11,11 @@ import kotlinx.coroutines.launch
 
 class SplashScreenActivity : AppCompatActivity() {
 
-    lateinit var animationDrawable: AnimationDrawable
     override fun onCreate(savedInstanceState: Bundle?) {
-        val SPLASH_DELAY: Long = 3000
+        val SPLASH_DELAY: Long = 2000
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        val loading = findViewById<ImageView>(R.id.loadingIndicator)
-        animationDrawable = loading.drawable as AnimationDrawable
-        animationDrawable.start()
 
         CoroutineScope(Dispatchers.Main).launch {
             delay(SPLASH_DELAY)
