bfae66ae1dc849972d7bdbc0bac0e9d0492c923b
Fariha Sultana <farihasultana237@gmail.com>
2023-09-17 14:18:33 +0600

    Back Button works on Subscription Fragment

--- app/src/main/java/com/example/rtv_plus_android_app_revamp/ui/fregments/SubscriptionFragment.kt
+++ app/src/main/java/com/example/rtv_plus_android_app_revamp/ui/fregments/SubscriptionFragment.kt
@@ -3,21 +3,36 @@ package com.example.rtv_plus_android_app_revamp.ui.fregments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.Navigation.findNavController
 import com.example.rtv_plus_android_app_revamp.R
-import com.example.rtv_plus_android_app_revamp.databinding.FragmentSearchBinding
 import com.example.rtv_plus_android_app_revamp.databinding.FragmentSubscriptionBinding
 
-class SubscriptionFragment : Fragment() {
+class SubscriptionFragment : Fragment(){
     private lateinit var binding: FragmentSubscriptionBinding
+    private lateinit var toolBar: Toolbar
+    private lateinit var actionBar: ActionBar
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSubscriptionBinding.inflate(inflater, container, false)
+        //toolBar = binding.toolbar;
+        //toolBar.title = "Subscribe"
+        val toolBarIconSubscribe = binding.toolBarIconSubscribe
+        toolBarIconSubscribe.setOnClickListener {
+            val navController = findNavController(binding.root)
+            navController.navigate(R.id.HomeFragment)
+        }
+
         return binding.root
     }
+
+
 
 
 }
