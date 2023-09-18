699801ae2d699d34a94b9b1f9ae99f55ce98c4b5
Eatiak <easourav@gmail.com>
2023-09-18 11:21:26 +0600

    code indent

--- app/src/main/java/com/example/rtv_plus_android_app_revamp/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/example/rtv_plus_android_app_revamp/ui/activities/SplashScreenActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rtv_plus_android_app_revamp.ui.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,31 +12,30 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.Calendar
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
 
-    lateinit var calendar: Calendar
-    lateinit var footer: TextView
+    private lateinit var calendar: Calendar
+    private lateinit var footer: TextView
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
-        val SPLASH_DELAY: Long = 2000
+        val splashDelay: Long = 2000
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         calendar = Calendar.getInstance()
-        var year : Int = calendar.get(Calendar.YEAR)
+        val year: Int = calendar.get(Calendar.YEAR)
 
         footer = findViewById(R.id.footer)
         footer.text = "©RTV & EBS $year. All Rights Reserved"
 
-
         CoroutineScope(Dispatchers.Main).launch {
-            delay(SPLASH_DELAY)
+            delay(splashDelay)
 
             val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
             startActivity(intent)
-
-
             finish()
         }
-
     }
 }
