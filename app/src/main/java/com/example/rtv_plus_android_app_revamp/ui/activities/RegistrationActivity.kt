6250555de325b9ca060df89bef9d97ee850c9f3f
Fariha Sultana <farihasultana237@gmail.com>
2023-09-25 11:37:16 +0600

    Registration UI

---
+++ app/src/main/java/com/example/rtv_plus_android_app_revamp/ui/activities/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rtv_plus_android_app_revamp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rtv_plus_android_app_revamp.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
